b82556e20c4b533eeb5717ff20d23bdba3e9d0b1
m.saplel <kaebs19@gmail.com>
2023-08-11 19:46:45 +0300

    1.1
    Frist version

---
+++ passingData/Controllers/FristVC.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  passingData
+//
+//  Created by mjeed on 10/08/2023.
+//
+
+import UIKit
+
+class FristVC: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var emailLable: UILabel!
+    @IBOutlet weak var genderLable: UILabel!
+    @IBOutlet weak var cuntryLable: UILabel!
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // MARK: - View Life Cycle
+        getDataUser()
+
+        
+    }
+    // MARK: -  Configure Action - Func
+    
+    func getDataUser() {
+        let name = UserDefaults.standard.string(forKey: "name") ?? ""
+        let email = UserDefaults.standard.string(forKey: "email") ?? ""
+        let gender = UserDefaults.standard.string(forKey: "gender") ?? ""
+        let cuntry = UserDefaults.standard.string(forKey: "cuntry") ?? ""
+        print("Your name\(name) , your email\(email) ,your gender\(gender) , your country \(cuntry)")
+        
+        // عرض البيانات داخل الـ Labels
+        nameLable.text = "Name: \(name)"
+        emailLable.text = "Email: \(email)"
+        genderLable.text = "Gender: \(gender)"
+        cuntryLable.text = "Country: \(cuntry)"
+
+    }
+
+    @IBAction func updateProfileButton(_ sender: UIButton) {
+        
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "SecondVC") as? SecondVC else {return}
+        vc.modalTransitionStyle = .flipHorizontal
+        vc.modalPresentationStyle = .fullScreen
+        vc.userdata = self
+        present(vc, animated: true)
+    }
+    
+}
+
+extension FristVC : Profile {
+    /// protocol get data from SecondVC
+    func passDataProfile(name: String, email: String, gender: String, cuntry: String) {
+        
+        nameLable.text = name
+        emailLable.text = email
+        genderLable.text = gender
+        cuntryLable.text = cuntry
+        
+        
+    }
+
+}
---
+++ passingData/Controllers/SecondVC.swift
@@ -0,0 +1,45 @@
+//
+//  SecondVC.swift
+//  passingData
+//
+//  Created by mjeed on 10/08/2023.
+//
+
+import UIKit
+
+protocol Profile {
+    func passDataProfile(name:String , email:String , gender:String , cuntry: String)
+}
+
+class SecondVC: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var genderTextField: UITextField!
+    @IBOutlet weak var cuntryTextField: UITextField!
+
+    var userdata:Profile?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // MARK: - View Life Cycle
+        
+        
+    }
+    
+    // MARK: -  Configure Action - Func
+
+    @IBAction func updatePressBution(_ sender: UIButton) {
+        
+        userdata?.passDataProfile(name: nameTextField.text!, email: emailTextField.text!, gender: genderTextField.text!, cuntry: cuntryTextField.text!)
+        
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(emailTextField.text, forKey: "email")
+        UserDefaults.standard.set(genderTextField.text, forKey: "gender")
+        UserDefaults.standard.set(cuntryTextField.text, forKey: "cuntry")
+        
+        dismiss(animated: true)
+    }
+    
+
+}
